852553843ce1d8d712074d0a8152c86a12fb123d
Romain Lt <romain.letouze@s3pweb.com>
2022-04-05 16:26:23 +0200

    fix(convert): fix incrementing of seconds after 60min and 24h, and improve code

--- src/convert.js
+++ src/convert.js
@@ -1,9 +1,16 @@
-// Astronomical format (365.25days)
-function getAstrYears (duration) {
-  const oneYearInSeconds = 31557600
-  const rest = duration % oneYearInSeconds
-  const durationSeconds = duration - rest
-  const years = durationSeconds / oneYearInSeconds
+const yearInSecondsAstr = 31557600 // 31,557,600 seconds (365.25 days)
+const yearInSecondsCal = 31536000 // 31,536,000 seconds (365 days)
+
+/**
+ * Get the years count
+ * @param {*} duration
+ * @param {*} yearInSeconds
+ * @returns
+ */
+function getYears (duration, yearInSeconds) {
+  const rest = duration % yearInSeconds
+  const durationInSeconds = duration - rest
+  const years = durationInSeconds / yearInSeconds
 
   return {
     years,
@@ -11,21 +18,12 @@ function getAstrYears (duration) {
   }
 }
 
-// Calendar format (365 days)
-function getCalYears (duration) {
-  const oneYearInSeconds = 31536000
-  const rest = duration % oneYearInSeconds
-  const durationSeconds = duration - rest
-  const years = durationSeconds / oneYearInSeconds
-
-  return {
-    years,
-    rest
-  }
-}
-
-// Duration calculation to human format
-module.exports = function convert (duration, format) {
+/**
+ * Duration calculation to human format
+ * @param {*} duration in seconds
+ * @param {*} format 'cal' or 'astr'
+ */
+module.exports = function convert(duration, format) {
   const obj = {
     years: 0,
     days: 0,
@@ -35,12 +33,12 @@ module.exports = function convert (duration, format) {
   }
 
   // if duration > 1 year : calculate years before the rest
-  if (format === 'astr' && duration > 31557599) {
-    const res = getAstrYears(duration)
+  if (format === 'astr' && duration >= yearInSecondsAstr) {
+    const res = getYears(duration, yearInSecondsAstr)
     obj.years = res.years // Get years
     duration = res.rest // Get rest
-  } else if (format === 'cal' && duration > 31535999) {
-    const res = getCalYears(duration)
+  } else if (format === 'cal' && duration >= yearInSecondsCal) {
+    const res = getYears(duration, yearInSecondsCal)
     obj.years = res.years
     duration = res.rest
   }
@@ -52,20 +50,18 @@ module.exports = function convert (duration, format) {
     obj.minutes = 1
     obj.seconds = 0
   } else {
-    // MINUTES
+    /// MINUTES
     const restSeconds = duration % 60 // Rest in secondes
     const durationSeconds = duration - restSeconds // Duration - rest
     const minutes = durationSeconds / 60 // Total number of minutes
 
     if (minutes < 60) {
       obj.minutes = minutes
-
-      if (restSeconds > 0) {
-        obj.seconds = restSeconds
-      }
+      obj.seconds = restSeconds
     } else if (minutes === 60) {
       obj.hours = 1
       obj.minutes = 0
+      obj.seconds = restSeconds
     } else {
       /// HOURS
       const restMinutes = minutes % 60
@@ -79,6 +75,7 @@ module.exports = function convert (duration, format) {
       } else if (hours === 24) {
         obj.days = 1
         obj.hours = 0
+        obj.seconds = restSeconds
       } else {
         /// DAYS
         const restHours = hours % 24
